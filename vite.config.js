b0b70984648e968a3438965ac1de4cde8b8f14de
Antonio Mora <j_antonio_mora@hotmail.com>
2026-05-04 21:48:52 -0600

    se agrega el vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: "public", // asegura que sw.js, manifest.json e íconos se copien al build
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+});
